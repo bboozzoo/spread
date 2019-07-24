d78e065293951e421f9c3f21d183664a12417254
Maciek Borzecki <maciek.borzecki@gmail.com>
2019-07-24 11:11:44 +0200

    spread/qemu: use more than 1 CPU
    Default to 4 CPUs, unless the host has fewer.
    
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- spread/qemu.go
+++ spread/qemu.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
 
 	"golang.org/x/net/context"
@@ -114,7 +115,11 @@ func (p *qemuProvider) Allocate(ctx context.Context, system *System) (Server, er
 	serial := fmt.Sprintf("telnet:127.0.0.1:%d,server,nowait", port+100)
 	monitor := fmt.Sprintf("telnet:127.0.0.1:%d,server,nowait", port+200)
 	fwd := fmt.Sprintf("user,hostfwd=tcp:127.0.0.1:%d-:22", port)
-	cmd := exec.Command("kvm", "-snapshot", "-m", strconv.Itoa(mem), "-net", "nic", "-net", fwd, "-serial", serial, "-monitor", monitor, path)
+	cpus := 4
+	if systemCPUs := runtime.NumCPU(); systemCPUs < cpus {
+		cpus = systemCPUs
+	}
+	cmd := exec.Command("kvm", "-snapshot", "-smp", strconv.Itoa(cpus), "-m", strconv.Itoa(mem), "-net", "nic", "-net", fwd, "-serial", serial, "-monitor", monitor, path)
 	if os.Getenv("SPREAD_QEMU_GUI") != "1" {
 		cmd.Args = append([]string{cmd.Args[0], "-nographic"}, cmd.Args[1:]...)
 	}
